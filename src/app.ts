3dc53011d92014bbdf7c8c5611fe1dc5d35c7674
sumeet.rana <sumeetsinh28@gmail.com>
2022-09-29 15:47:51 +0530

    database connected

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 
+import databaseConnection from './setupDatabase';
 import { SocialRackServer } from "./setupServer";
 
 class Application {
   public initialize(): void {
+    databaseConnection();
     const app: Express = express();
     const server: SocialRackServer = new SocialRackServer(app);
     server.start();
